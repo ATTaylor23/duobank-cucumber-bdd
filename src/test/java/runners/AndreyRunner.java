f3f72b3bbbd4cef1a047a87d25addbb655a66be4
Andrey O <opexandrey@gmail.com>
2022-05-02 23:49:35 -0400

    1st commit

--- src/test/java/runners/AndreyRunner.java
+++ src/test/java/runners/AndreyRunner.java
@@ -1,4 +1,32 @@
 package runners;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+
+        tags = "@temp",
+
+        features = "src/test/resources",
+
+        glue = "stepDefinitions",
+
+        stepNotifications = true,
+
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+
+        plugin = {"pretty",
+                "html:target/duobank-report/built-in-report.html",
+                "json:target/Cucumber.json",
+                "rerun:target/failed.txt"
+        }
+
+        //,dryRun = true
+)
+
 public class AndreyRunner {
 }
